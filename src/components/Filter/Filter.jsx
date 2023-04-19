1f6fc0f1e3e7e48fd791f54de5c39f1443b12e90
Kate2610 <yekaterina.anisimova@gmail.com>
2023-04-19 14:53:35 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,9 @@
-import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { connect } from 'react-redux';
 import { setFilter } from 'redux/contacts/slice';
 import { getFilter } from 'redux/contacts/selectors';
 import s from './Filter.module.css';
 
-export default function Filter() {
-  const dispatch = useDispatch();
-  const filter = useSelector(getFilter);
-
+function Filter({ filter, setFilter }) {
   return (
     <label className={s.label}>
       Find contacts by name
@@ -16,9 +13,17 @@ export default function Filter() {
         name="filter"
         value={filter}
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        onChange={evt => dispatch(setFilter(evt.target.value))}
+        onChange={evt => setFilter(evt.target.value)}
         required
       />
     </label>
   );
 }
+
+const mapStateToProps = state => ({
+  filter: getFilter(state)
+});
+
+const mapDispatchToProps = { setFilter };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
